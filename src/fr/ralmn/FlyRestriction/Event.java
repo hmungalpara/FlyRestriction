b8e53e81651f2e4ae1284d0bebc81b980439fcfe
ralmn <ranger_tigre@hotmail.fr>
2012-03-20 18:38:01 +0100

    1.1
    Retret de la prise en compte de Y dans la vitesse
    
    
    Signed-off-by: ralmn <ranger_tigre@hotmail.fr>

--- src/fr/ralmn/FlyRestriction/Event.java
+++ src/fr/ralmn/FlyRestriction/Event.java
@@ -2,6 +2,7 @@ package fr.ralmn.FlyRestriction;
 
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -12,45 +13,45 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class Event implements Listener {
 
 	Main plugin;
-	
-	public Event(Main m){
+
+	public Event(Main m) {
 		plugin = m;
 	}
-	
+
 	@EventHandler(priority = EventPriority.MONITOR)
-	public void onPlayerSpeedHack(PlayerMoveEvent e){
+	public void onPlayerSpeedHack(PlayerMoveEvent e) {
 		Player pl = e.getPlayer();
-		boolean a = (e.getFrom().getBlockY() > e.getTo().getBlockY());
-		if(a)
-			return;
-		if(!(pl.getGameMode() == GameMode.CREATIVE) && !plugin.canRun(pl.getName())){
-			double speed = e.getFrom().distance(e.getTo()) * 5;
-			if((int)speed > plugin.getConfig().getInt("settings.runmax")){
-				pl.kickPlayer(ChatColor.RED +  plugin.getConfig().getString("message.kick.run"));
+
+		Location from = new Location(e.getFrom().getWorld(),
+				e.getFrom().getX(), 0, e.getFrom().getZ());
+		Location to = new Location(e.getTo().getWorld(), e.getTo().getX(), 0, e
+				.getTo().getZ());
+		
+
+		if (!(pl.getGameMode() == GameMode.CREATIVE)
+				&& !plugin.canRun(pl.getName())) {
+			double speed = from.distance(to) * 5;
+			if ((int) speed > plugin.getConfig().getInt("settings.runmax")) {
+				pl.kickPlayer(ChatColor.RED
+						+ plugin.getConfig().getString("message.kick.run"));
 			}
 		}
-		
+
 	}
-	
-	
+
 	@EventHandler(priority = EventPriority.MONITOR)
-	public void onPlayerJoin(PlayerJoinEvent e){
+	public void onPlayerJoin(PlayerJoinEvent e) {
 		Player pl = e.getPlayer();
-		
+
 		boolean a = plugin.canFly(pl.getName());
-		
-		
+
 		pl.setAllowFlight(a);
-		
-		if(a)
+
+		if (a)
 			plugin.log.info(pl.getName() + " peut voler");
 		else
 			plugin.log.info(pl.getName() + " ne peux pas voler");
-		
-		
-		
+
 	}
-	
-	
-	
+
 }
